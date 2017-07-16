189242beeccb6cddc6f050cc69d38130e8ed594f
Yunkie88 <yunkie.vnet@gmail.com>
2017-07-16 14:39:07 +0800

    filter by date range

--- bill.js
+++ bill.js
@@ -1,5 +1,5 @@
 var express = require('express');
-var moment = require('moment');
+var moment = require ('moment');
 var router = express.Router();
 
 
@@ -9,5 +9,47 @@ res.json({ message: 'Hooray! Good Paymaster From Now!' });
 
 module.exports = router;
 
-moment().format('YYYY MM DD');
-moment().fromNow();
+var Bill = require ('./billschema.js');
+
+router.route('/bills')
+.post(function(req, res) {
+var bill = new Bill();
+bill.name = req.body.name;
+bill.amount = req.body.amount;
+bill.datedue = req.body.datedue;
+book.save(function(err) {
+if (err)
+res.send(err);
+res.json({ message: 'Bill created!' });
+});
+});
+
+router.route ('/bills')
+.get(function(req,res){
+	var timestamp = req.params.start;
+	if (moment(timestamp).isValid()){
+		start = moment(timestamp);
+	}
+
+	else {
+		start = moment(parseInt(timestamp)*1000);
+	};
+	
+	var timestamp1 = req.params.end;
+	if (moment(timestamp1).isValid()){
+		end = moment(timestamp1);
+	}
+
+	else {
+		end = moment(parseInt(timestamp1)*1000);
+	}
+	
+
+Bill.find ({
+	datedue: {$gte:start,$lte:end}
+}).exec (function (err,bills){
+	if (err)
+	res.send(err);
+	res.json(bills);
+});
+});
