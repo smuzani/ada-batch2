928a1bc5d7eae6adcf43d411587645d8b8514f37
david kuek <davidkuek1990@gmail.com>
2017-07-16 17:06:36 +0800

    added delete button

--- view_all.js
+++ view_all.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
 	$('#datetimepicker1').datetimepicker();
+	$('#datetimepicker2').datetimepicker();
 });
+
+function deleteRow(btn) {
+  var row = btn.parentNode.parentNode;
+  row.parentNode.removeChild(row);
+}
+
