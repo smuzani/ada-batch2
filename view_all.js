65d72ad3848e2e7811a13c0b7ece3a3480cbd852
david kuek <davidkuek1990@gmail.com>
2017-07-23 12:25:54 +0800

    added delete function wip

--- view_all.js
+++ view_all.js
@@ -1,10 +1,28 @@
+var url = "http://localhost:8000"
+
 $(document).ready(function(){
 	$('#datetimepicker1').datetimepicker();
 	$('#datetimepicker2').datetimepicker();
+	$(".delete").click(deleteItem);
+
+function deleteItem(){
+	alert('button clicked')
+	$.ajax({
+    url: 'http://localhost:8000/bill/:id',
+    type: 'DELETE',
+    success: function(){
+    	alert('Bill deleted');
+    },
+    error: function(){
+    	alert('Error');
+    }
+});
+}
+
 });
 
 function newitem () {
-	var name = ...;
+	var name = "";
 	var dueDate = "";
 	var text = '<td>1</td><td>' + name + '</td><td>'+ dueDate + '</td><td>RM10</td><td>High</td><td><button type="button"  class="btn btn-pencil" aria-label="Left Align"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></button><button type="button" class="btn btn-trash" aria-label="Left Align"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button></td>'
 	for (var i = list.length - 1; i >= 0; i--) {
@@ -12,8 +30,5 @@ function newitem () {
 	}
 }
 
-function deleteRow(btn) {
-  var row = btn.parentNode.parentNode;
-  row.parentNode.removeChild(row);
-}
+
 
